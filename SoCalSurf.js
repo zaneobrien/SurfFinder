6145a4facbf3d4e44e6ef422b2ac9b620c433836
Zane OBrien <zaneobr@gmail.com>
2018-01-17 08:42:11 -0800

    added secrets and Client

--- SoCalSurf.js
+++ SoCalSurf.js
@@ -1,8 +1,14 @@
-//Initial Commit Outline
+import APIKEY from secrets
 
 //TODO:use msw-client to get spot live information for each spot
 const MSW = require('msw-client');
 const MswClient = new MSW({
-    apikey: 'YOUR_API_KEY',
+    apikey: APIKEY,
     spot_id: 2 // must be a number
+});
+
+MswClient.request().then(data => {
+  console.log(data);
+}).catch(err => {
+  console.log(err);
 });
